43b54f7012d06cd0bed2971369dca1c1d081fc5d
Atul Nirbhavane <anirbhavane106@gmail.com>
2024-05-21 00:30:12 +0530

    initial commit

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using ProducerApplication.Models;
+
+namespace ProducerApplication.Controllers
+{
+    public class CarsController : ControllerBase
+    {
+        private ProducerConfig _configuration;
+        private readonly IConfiguration _config;
+
+        public CarsController(ProducerConfig configuration, IConfiguration config)
+        {
+            _configuration = configuration; 
+            _config = config;
+        }
+
+        [HttpGet]
+        public string Ping()
+        {
+            return "Cars Pinged";
+        }
+
+        [HttpPost("sendBookingDetails")]
+        public async Task<ActionResult> Get([FromBody] CarDetails car)
+        {
+            var topic = _config.GetSection("TopicName").Value;
+
+            string serializedData = Newtonsoft.Json.JsonConvert.SerializeObject(car);
+
+            using(var producer = new ProducerBuilder<Null, string>(_configuration).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedData });
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                return Ok(true);
+
+            }
+        }
+
+    }
+}
---
+++ Models/CarDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace ProducerApplication.Models
+{
+    public class CarDetails
+    {
+        public int CarId { get; set; }
+        public string CarName { get; set; }
+        public string BookingStatus { get; set; }
+    }
+}
